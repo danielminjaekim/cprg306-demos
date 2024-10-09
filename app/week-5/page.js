5946ac45022f5e90ca28ad70e064561457d62ffb
Daniel Kim <151947516+danielminjaekim@users.noreply.github.com>
2024-10-09 12:28:53 -0600

    Completed Week 5 Demo

---
+++ app/week-5/page.js
@@ -0,0 +1,10 @@
+import DogForm from "./dog-form";
+
+export default function Page() {
+  return (
+    <div className="m-4">
+      <h1 className="text-4xl m-2 bold">Week 5: Interactivity with Forms</h1>
+      <DogForm />
+    </div>
+  );
+}
